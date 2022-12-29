9fb0063c280c3cd7914569fbd600a520102e0f12
jasarmig <N0seasap@ksozr>
2022-12-29 11:28:43 -0500

    configured dependencies

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const allureWriter = require('@shelex/cypress-allure-plugin');
+
+module.exports = {
+  
+  "defaultCommandTimeout" : 3000,
+  "env" : {
+    "TAGS" : "not @ignore",
+    "allure" : true
+  },
+
+  e2e : {
+    baseUrl : "https://www.demoblaze.com",
+    specPattern : "**/*.feature",
+    setupNodeEvents(on, config) {
+      on("file:preprocessor", cucumber())
+      allureWriter(on, config);
+      return config;
+    }
+  }
+}
